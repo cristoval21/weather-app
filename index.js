2e183bbb45cf89797020353b39dc64a656d1b7d9
Cristoval Wenoch <wenochcristoval@gmail.com>
2024-05-19 21:11:06 +0700

    wip

--- index.js
+++ index.js
@@ -6,37 +6,60 @@ class Weather {
 
   async getData() {
     const response = await fetch(
-      'https://api.weatherapi.com/v1/current.json?key=eff8835c269b4d5b910101327241205&q=' +
-        encodeURIComponent(this._cityName)
+      'https://api.weatherapi.com/v1/forecast.json?key=eff8835c269b4d5b910101327241205&q=' +
+        encodeURIComponent(this._cityName) +
+        '&days=3'
     );
     const data = await response.json();
     return data;
   }
 
-  async getLocationData() {
+  async getLocation() {
     const data = await this._cityData;
-    return data.location;
+    const location = data.location;
+    return {
+      name: location.name,
+      region: location.region,
+      country: location.country,
+    };
   }
 
-  async getCurrentData() {
+  async getCurrentWeather() {
     const data = await this._cityData;
-    return data.current;
-  }
-
-  async getConditionData() {
-    const data = await this.getCurrentData();
-    return data.condition;
+    const currentData = data.current;
+    return {
+      temperature: currentData.temp_c,
+      icon: currentData.condition.icon,
+      condition: currentData.condition.text,
+      date: currentData.last_updated,
+    };
   }
 }
 
 const $searchForm = document.getElementById('search-form');
-const $searchInput = document.getElementById('search-input');
-const $searchButton = document.getElementById('search-button');
+$searchForm.addEventListener('submit', handleSearch);
 
-$searchButton.addEventListener('click', (e) => {
+function handleSearch(e) {
+  const $searchForm = document.getElementById('search-form');
+  const $searchInput = document.getElementById('search-input');
+
+  const $currentDate = document.getElementById('current-date');
+  const $currentIcon = document.getElementById('current-icon');
+  const $currentTemperature = document.getElementById('current-temperature');
+  const $currentCondition = document.getElementById('current-condition');
+
   if ($searchForm.checkValidity()) {
     e.preventDefault();
+
     const weather = new Weather($searchInput.value);
-    weather.getLocationData().then((res) => console.log(res));
+    weather.getLocation().then((city) => {
+      $searchInput.value = `${city.name}, ${city.country}`;
+    });
+    weather.getCurrentWeather().then((current) => {
+      $currentDate.textContent = current.date;
+      $currentIcon.src = current.icon;
+      $currentTemperature.textContent = current.temperature;
+      $currentCondition.textContent = current.condition;
+    });
   }
-});
+}
